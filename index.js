f5bef953578c59befa76f9bdd39141af4fefbd3b
yzhuangintcd <127947054+yzhuangintcd@users.noreply.github.com>
2024-05-06 18:11:15 +0100

    added new style to the nav bar nav bar

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log(`Hello, Testing`);
+
+window.onscroll = function() {stickyNavBar()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function stickyNavBar() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+    console.log("Sticking");
+  } else {
+    navbar.classList.remove("sticky");
+    console.log("No Sticking");
+  }
+}
