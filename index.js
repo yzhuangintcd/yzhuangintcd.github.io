0bb5cb6d0cbf7f819e4b5d647e87816251ee343d
yzhuangintcd <127947054+yzhuangintcd@users.noreply.github.com>
2024-05-06 19:03:08 +0100

    Update Home page with new menu

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 
-window.onscroll = function() {stickyNavBar()};
+// window.onscroll = function() {stickyNavBar()};
 
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
+// var navbar = document.getElementById("navbar");
+// var sticky = navbar.offsetTop;
 
-function stickyNavBar() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-    console.log("Sticking");
-  } else {
-    navbar.classList.remove("sticky");
-    console.log("No Sticking");
-  }
-}
+// function stickyNavBar() {
+//   if (window.pageYOffset >= sticky) {
+//     navbar.classList.add("sticky")
+//     console.log("Sticking");
+//   } else {
+//     navbar.classList.remove("sticky");
+//     console.log("No Sticking");
+//   }
+// }
